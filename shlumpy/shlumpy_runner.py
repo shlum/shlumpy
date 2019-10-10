b48716f6690da5982da33af241fb6a3cb8e75342
DorElias <DorElias1@gmail.com>
2019-10-10 22:04:23 +0300

    stub code

---
+++ shlumpy/shlumpy_runner.py
@@ -0,0 +1,15 @@
+class ShlumpyRunner:
+    def __init__(self):
+        pass
+
+    def add_flow(self, flow):
+        pass
+
+    def run_server(self):
+        pass
+
+    def run_flow_by_name(self, flow_name):
+        pass
+
+    def close(self):
+        pass
